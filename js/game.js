8534318b13616eb1e65e979fd434de96eba15d4b
Nick Yee <njsyee@gmail.com>
2016-12-09 09:35:29 +0100

    Added character sprites and textures

--- js/game.js
+++ js/game.js
@@ -1,16 +1,15 @@
 "use strict";
 var game = new Phaser.Game(800, 600, Phaser.AUTO, "", { preload: preload, create: create, update: update });
+var player;
+var platforms;
 
 function preload () {
- game.load.image('sky', 'assets/sky.png');
- game.load.image('ground', 'assets/platform.png');
- game.load.image('star', 'assets/star.png');
- game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+ game.load.image('sky', 'assets/images/textures/sky.png');
+ game.load.spritesheet('player', 'assets/images/characters/sprites-funny-bunny-ios-isometric-tiles.jpg', 32, 48);
 
 }
 
-var player;
-var platforms;
+
 
 function create () {
 
